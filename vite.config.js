1e2624138a6e4805a6f4df3f79c6982ba233c721
yjcq <yjcq007@gmail.com>
2023-11-22 22:27:34 -0400

    videoclub ready

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: resolve('./public/build'),
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]'
+            }
+        }
+    },
     plugins: [
         laravel({
             input: [
